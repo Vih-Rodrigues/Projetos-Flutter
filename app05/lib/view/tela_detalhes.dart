377be67c0693ff6bd6d52e5e0ee19f286b44f1e8
Vitoria Rodrigues <andrader.vitoria@gmail.com>
2022-04-06 11:55:39 -0300

    App05 atualizado - troca de tela concluida

--- app05/lib/view/tela_detalhes.dart
+++ app05/lib/view/tela_detalhes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../model/paises.dart';
 
 class TelaDetalhes extends StatefulWidget {
   const TelaDetalhes({ Key? key }) : super(key: key);
@@ -10,11 +11,36 @@ class TelaDetalhes extends StatefulWidget {
 class _TelaDetalhesState extends State<TelaDetalhes> {
   @override
   Widget build(BuildContext context) {
+
+    //
+    // RECUPERAR OS DADOS PASSADOS COMO ARGUMENTO
+    //
+    final Pais p = 
+      ModalRoute.of(context)!.settings.arguments as Pais;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('IBGE'),
         backgroundColor: Colors.black87,
       ),
+
+      //
+      // BODY
+      //
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: [
+            Image.network(
+              'https://countryflagsapi.com/png/' +
+              p.abreviatura.toLowerCase(),
+            ),
+            Text(p.nome, style: const TextStyle(fontSize: 28)),
+            Text(p.historico, style: const TextStyle(fontSize: 20))
+          ],
+        ),
+      ),
+
       backgroundColor: Colors.white24,
       //
       // Floating Action Button
